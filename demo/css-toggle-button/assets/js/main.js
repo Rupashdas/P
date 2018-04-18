268ebfd14722ef791c5e5ee3fd879f945003ef4a
Rupash Das <rupash.das.40@gmail.com>
2018-04-19 03:46:28 +0600

    Add files via upload

---
+++ demo/css-toggle-button/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       
+        
+        $(".button").on("click", function(){
+            $(".button").addClass("active");
+            $(this).removeClass("active");
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
